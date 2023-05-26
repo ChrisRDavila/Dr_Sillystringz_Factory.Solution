39f7f0a7bc2cb3562cdbf86c344165bf639792b0
ChrisRDavila <crossdavila@gmail.com>
2023-05-26 11:32:46 -0700

    update splash

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
-
-
+using Factory.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index() 
-    { 
-      return View(); 
+    public class HomeController : Controller
+    {
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        Engineer[] engineers = _db.Engineers.OrderBy(engineers => engineers.Name).ToArray();
+        Machine[] machines = _db.Machines.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("engineers", engineers);
+        model.Add("machines", machines);
+
+        return View(model);
+      }
+
     }
-  }
 }
