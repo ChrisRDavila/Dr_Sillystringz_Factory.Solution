a10e4228d879b85d4292bfca86e4fea3845e5715
ChrisRDavila <crossdavila@gmail.com>
2023-05-26 13:05:02 -0700

    still having issue connecting to index page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -18,14 +18,14 @@ namespace Factory.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      Engineer[] engineers = _db.Engineers.OrderBy(engineers => engineers.Name).ToArray();
+      Engineer[] engineers = _db.Engineers.ToArray();
       Machine[] machines = _db.Machines.ToArray();
       Dictionary<string,object[]> model = new Dictionary<string, object[]>();
       model.Add("engineers", engineers);
       model.Add("machines", machines);
-
       return View(model);
     }
 
   }
 }
+//.OrderBy(engineers => engineers.Name)
