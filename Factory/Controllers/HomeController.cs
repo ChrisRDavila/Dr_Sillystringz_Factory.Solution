d1889cce99edeb14a4aa21191887efdddda5b4f9
ChrisRDavila <crossdavila@gmail.com>
2023-05-26 11:51:24 -0700

    problems running app, connecting to Eng Index, WIP troubleshooting

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -6,26 +6,26 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
     {
-      private readonly FactoryContext _db;
-
-      public HomeController(FactoryContext db)
-      {
-        _db = db;
-      }
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        Engineer[] engineers = _db.Engineers.OrderBy(engineers => engineers.Name).ToArray();
-        Machine[] machines = _db.Machines.ToArray();
-        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
-        model.Add("engineers", engineers);
-        model.Add("machines", machines);
-
-        return View(model);
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Engineer[] engineers = _db.Engineers.OrderBy(engineers => engineers.Name).ToArray();
+      Machine[] machines = _db.Machines.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("engineers", engineers);
+      model.Add("machines", machines);
+
+      return View(model);
+    }
+
+  }
 }
