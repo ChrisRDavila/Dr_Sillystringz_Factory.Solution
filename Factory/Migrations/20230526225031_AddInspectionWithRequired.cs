ff01c2d716671de70bd16160a01cbc51e7d8997b
ChrisRDavila <crossdavila@gmail.com>
2023-05-26 16:03:08 -0700

    Added inspection date property to Machines class

---
+++ Factory/Migrations/20230526225031_AddInspectionWithRequired.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddInspectionWithRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Inspection",
+                table: "Machines",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inspection",
+                table: "Machines");
+        }
+    }
+}
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace Factory.Models
 {
@@ -8,6 +9,8 @@ namespace Factory.Models
     public int MachineId { get; set; }
     [Required(ErrorMessage = "The machines model can't be empty.")]
     public string Model { get; set; }
+    [Required(ErrorMessage = "The previous inspection field can't be empty")]
+    public DateTime Inspection { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
 }
